700f302b57370b12c4e70cc875e804c3d7b3eb85
0xdreamerr <0xdreamerr@gmail.com>
2024-10-22 14:42:38 +0300

    Added and deployed OFT Contracts

---
+++ script/DeployOFT.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {EthOFT} from "../src/EthOFT.sol";
+import {ArbOFT} from "../src/ArbOFT.sol";
+
+
+contract DeployOFTScript is Script {
+
+    function setUp() public {
+
+    }
+
+    function run() public {
+        
+}
---
+++ src/ArbOFT.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+import { Ownable } from "openzeppelin-contracts/contracts/access/Ownable.sol";
+import { OFT } from "layerzero-v2/packages/layerzero-v2/evm/oapp/contracts/oft/OFT.sol";
+
+/// @notice OFT is an ERC-20 token that extends the OFTCore contract.
+contract ArbOFT is OFT {
+    string oftName = "Gelasimoff ARB";
+    string oftSymbol = "GLMF";
+    address lzEndpoint = 0x6EDCE65403992e310A62460808c4b910D972f10f;
+
+    constructor() OFT(oftName, oftSymbol, lzEndpoint, msg.sender) Ownable(msg.sender) {
+        _mint(msg.sender, 100 ether);
+    }
+}
