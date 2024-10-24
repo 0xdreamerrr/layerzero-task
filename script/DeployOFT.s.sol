280db458d287cd37d00d684730fbae8376ddfdc6
0xdreamerr <0xdreamerr@gmail.com>
2024-10-24 15:46:05 +0300

    Final version. Added tests and verification.

--- script/DeployOFT.s.sol
+++ script/DeployOFT.s.sol
@@ -1,17 +1,61 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.27;
 
 import {Script, console} from "forge-std/Script.sol";
-import {EthOFT} from "../src/EthOFT.sol";
-import {ArbOFT} from "../src/ArbOFT.sol";
+import { OptOFT } from "../src/OptOFT.sol";
+import { ArbOFT } from "../src/ArbOFT.sol";
+import { SendParam, OFTReceipt, IOFT } from "@layerzerolabs/oft-evm/contracts/interfaces/IOFT.sol";
+import { OptionsBuilder } from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OptionsBuilder.sol";
+import { MessagingFee } from "@layerzerolabs/oapp-evm/contracts/oapp/OApp.sol";
 
 
 contract DeployOFTScript is Script {
 
-    function setUp() public {
+    address public layerZeroEndpoint = 0x6EDCE65403992e310A62460808c4b910D972f10f;
+    uint32 public optId = 40232;
+    uint32 public arbId = 40231;
 
+    string optUrl = vm.envString("OPT_URL");
+    string arbUrl = vm.envString("ARB_URL");
+    uint256 internal opt;
+    uint256 internal arb;
+    
+    using OptionsBuilder for bytes;
+
+
+    function setUp() public {
+        opt = vm.createFork(optUrl);
+        arb = vm.createFork(arbUrl);
     }
 
+
     function run() public {
+        uint256 privateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.selectFork(arb);
+        vm.startBroadcast(privateKey);
         
+        ArbOFT aOFT = new ArbOFT("SuperFinal ArbOFT", "AOFT", layerZeroEndpoint, msg.sender);
+        console.log("ARB contract deployed at: ", address(aOFT));
+
+        vm.stopBroadcast();
+
+        vm.selectFork(opt);
+        vm.startBroadcast(privateKey);
+
+        OptOFT oOFT = new OptOFT("SuperFinal OptOFT", "OOFT", layerZeroEndpoint, msg.sender);
+        console.log("OPT contract deployed at: ", address(oOFT));
+
+        oOFT.setPeer(arbId, bytes32(uint256(uint160(address(aOFT)))));
+
+        vm.stopBroadcast();
+
+        vm.selectFork(arb);
+        vm.startBroadcast(privateKey);
+
+        aOFT.setPeer(optId, bytes32(uint256(uint160(address(oOFT)))));
+        vm.stopBroadcast();
+
+    }
 }
+
---
+++ test/SendOFT.t.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+// OApp imports
+import { IOAppOptionsType3, EnforcedOptionParam } from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OAppOptionsType3.sol";
+import { OptionsBuilder } from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OptionsBuilder.sol";
+import { OFTMock } from "devtools/examples/oft-adapter/test/mocks/OFTMock.sol";
+
+// OFT imports
+import { IOFT, SendParam, OFTReceipt } from "@layerzerolabs/oft-evm/contracts/interfaces/IOFT.sol";
+import { MessagingFee, MessagingReceipt } from "@layerzerolabs/oft-evm/contracts/OFTCore.sol";
+import { OFTMsgCodec } from "@layerzerolabs/oft-evm/contracts/libs/OFTMsgCodec.sol";
+import { OFTComposeMsgCodec } from "@layerzerolabs/oft-evm/contracts/libs/OFTComposeMsgCodec.sol";
+
+// OZ imports
+import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import { TestHelperOz5 } from "devtools/packages/test-devtools-evm-foundry/contracts/TestHelperOz5.sol";
+
+// Forge imports
+import "forge-std/console.sol";
+import {Test} from "forge-std/Test.sol";
+
+import {OptOFT} from "src/OptOFT.sol";
+import {ArbOFT} from "src/ArbOFT.sol";
+
+
+contract SendOFT is TestHelperOz5 {
+
+    using OptionsBuilder for bytes;
+
+    uint256 internal opt;
+    uint256 internal arb;
+
+    address private userA = address(0x1);
+    address private userB = address(0x2);
+    uint256 private initialBalance = 1000 ether;
+
+    uint32 private aEid = 1;
+    uint32 private oEid = 2;
+
+    ArbOFT private aOFT;
+    OptOFT private oOFT;
+
+    function setUp() public override{      
+        super.setUp();
+
+        setUpEndpoints(2, LibraryType.UltraLightNode);
+
+        vm.deal(userA, 1000 ether);
+        vm.deal(userB, 1000 ether);
+        
+        vm.prank(userA);
+        aOFT = ArbOFT(
+            _deployOApp(type(ArbOFT).creationCode, abi.encode("GL ARB", "GL", address(endpoints[aEid]), address(userA)))
+        );
+
+        vm.prank(userB);
+        oOFT = OptOFT(
+            _deployOApp(type(OptOFT).creationCode, abi.encode("GL OPT", "GL", address(endpoints[oEid]), address(userB)))
+        );
+
+        aOFT.mint(userA, initialBalance);
+        oOFT.mint(userB, initialBalance);
+    }
+
+
+    function test_constructor() public view{
+
+        assertEq(aOFT.owner(), userA);
+        assertEq(aOFT.token(), address(aOFT));
+        assertEq(aOFT.balanceOf(userA), initialBalance);
+
+        assertEq(oOFT.owner(), userB);
+        assertEq(oOFT.token(), address(oOFT));
+        assertEq(oOFT.balanceOf(userB), initialBalance);
+    }
+
+
+    function test_send_oft() public {
+        uint256 tokensToSend = 100 ether;
+        vm.deal(address(aOFT), 10 ether);
+
+        vm.prank(userB);
+        oOFT.setPeer(aEid, bytes32(uint256(uint160(address(aOFT)))));
+
+        vm.prank(userA);
+        aOFT.setPeer(oEid, bytes32(uint256(uint160(address(oOFT)))));
+
+
+        assertEq(aOFT.balanceOf(userA), initialBalance);
+        assertEq(oOFT.balanceOf(userB), initialBalance);
+
+        aOFT.mint(address(aOFT), 5000 ether);
+        vm.prank(userA);
+        aOFT.sendOFTtoOpt(tokensToSend, oEid, userB);
+
+
+        vm.prank(userA);
+        verifyPackets(oEid, addressToBytes32(address(oOFT)));
+
+
+        assertEq(aOFT.balanceOf(userA), initialBalance - tokensToSend);
+        assertEq(oOFT.balanceOf(userB), initialBalance + tokensToSend);
+    
+    }
+}
